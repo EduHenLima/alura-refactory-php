64dfcfe752f2a565c053b6fcf0a607b7328b6bca
Eduardo Lima <eduardoh.lima17@hotmai.com>
2020-12-07 00:03:15 -0300

    Tratando error con RunTimeException

--- pilha.php
+++ pilha.php
@@ -2,20 +2,27 @@
 
 function funcao1()
 {
-    echo 'Entrei na função 1' . PHP_EOL;
-    funcao2();
-    echo 'Saindo da função 1' . PHP_EOL;
+    try {
+        echo 'Entrei na função 1' . PHP_EOL;
+        funcao2();
+    } catch (RuntimeException $e){
+        echo "Deu erro na aplicação" . PHP_EOL;
+    }
 }
 
 function funcao2()
 {
+    $teste = new SplFixedArray(1);
     echo 'Entrei na função 2' . PHP_EOL;
-    for ($i = 1; $i <= 5; $i++) {
-        echo $i . PHP_EOL;
-    }
+    echo $teste[2] = 'valor';
+    echo 'Entrei na função 1' . PHP_EOL;
+
     echo 'Saindo da função 2' . PHP_EOL;
 }
 
 echo 'Iniciando o programa principal' . PHP_EOL;
 funcao1();
 echo 'Finalizando o programa principal' . PHP_EOL;
+
+git config --global user.email "eduardoh.lima17@hotmai.com"
+  git config --global user.name "Eduardo Lima"
