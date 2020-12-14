fbca3759cb9be2c62a248d59fc1bc2b6296f2b99
Eduardo Lima <eduardoh.lima17@hotmai.com>
2020-12-13 23:38:08 -0300

    Escriturando erros para producao

--- erros.php
+++ erros.php
@@ -1,6 +1,9 @@
 <?php
 
-const CONSTANTE = 'VALOR';
+error_reporting(E_ALL); // retornando todos os tipos de erros
+ini_set('display_erros', 1); // Relatando todos os erros
+ini_set('log_erros', 1); // Relatando todos os erros
+ini_set('error_log', '/logs'); // Procurar como criar arquivo de log
 
 // Erro fatal
 echo $var;
