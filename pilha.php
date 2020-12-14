92c10248418fc75b06646cf4753b487c34561b7e
Eduardo Lima <eduardoh.lima17@hotmai.com>
2020-12-13 22:33:15 -0300

    Exception generic

--- pilha.php
+++ pilha.php
@@ -5,20 +5,20 @@ function funcao1()
     try {
         echo 'Entrei na função 1' . PHP_EOL;
         funcao2();
-    } catch (RuntimeException | DivisionByZeroError $e){ // utilizando multi-catch
-        throw new \http\Exception\RuntimeException( 'Deu pau na bagaça',1 ,$e) ;
-        echo $e->getMessage() . PHP_EOL; // Retornando o erro viavel tento da exception
+    } catch (Throwable $e){ // utilizando multi-catch
+        throw new RuntimeException( 'Deu pau na bagaça',1 ,$e) ;
         echo $e->getLine() . PHP_EOL; // Qual linha estorou o erro;
         echo $e->getTraceAsString() . PHP_EOL; // Pilha de execução para chegar no mesmo
+    }catch (Error $erro){
+        echo $erro->getMessage();
     }
 }
 
 function funcao2()
 {
     echo 'Entrei da função 2' . PHP_EOL;
-    $exception = new RuntimeException();
-    throw $exception;
-    echo 'Saindo da função 2' . PHP_EOL;
+    $val = 1;
+        echo $val[0];
 }
 
 echo 'Iniciando o programa principal' . PHP_EOL;
